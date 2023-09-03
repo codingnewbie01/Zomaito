f6a29197779f4ce292b532bb037bc26613105448
Arvind <cod.ingnewbie01@gmail.com>
2023-09-03 19:16:28 +0530

    Img-Zomaito Folder
    A separate image folder created for all the images or videos used in the document

--- zomaito.js
+++ zomaito.js
@@ -1,4 +1,19 @@
-
+/*
 for (let i = 0; i <=10; i++) {
     console.log("Creating Zomato Clone😁");
     }
+function add(a, b){
+    let c=a+b;
+    console.log(c);
+}
+add(4,234); */
+
+const add1= function(a, b){
+    return a+b;
+}
+console.log(add1(14, 56));
+
+const mul =()=>{
+    console.log("This is an arrow function");
+}
+mul();
