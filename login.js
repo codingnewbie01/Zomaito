bf209a2f47acb5b43b6a3f1380f7a13434eb0f38
Arvind Verma <hey.arvind02@gmail.com>
2023-09-11 12:20:01 +0530

    inserted icons and logos in login page

---
+++ login.js
@@ -0,0 +1,9 @@
+let phn=document.querySelector('.phone > input');
+// let phnVal=phn.value;
+// console.log(phn);
+let phInp;
+phn.addEventListener('input', function(e){
+    // console.log(e.data);
+    phInp=phn.value;
+    console.log(phInp);
+})
